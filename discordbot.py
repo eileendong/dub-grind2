ed956cce49d6c2e1d9303807f1f1930f94a7e818
shanti1123 <shantidavis1123@gmail.com>
2025-01-25 11:45:55 -0800

    hi

--- discordbot.py
+++ discordbot.py
@@ -1,8 +1,5 @@
-<<<<<<< HEAD
-from click import command
 import discord 
-=======
-import discord 
+
 from discord.ext import commands
 
 intents = discord.Intents.default()
@@ -11,5 +8,3 @@ bot = commands.Bot(command_prefix = "~", intents = intents )
 
 async def command(ctx):
     await ctx.send()
-    
->>>>>>> 2d2343c13cf35f27bb0d16785a931f2a04f75c62
