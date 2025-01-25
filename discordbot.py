49a5652890e6c3069a9f65b8c6b6be8a82f06b40
anikatak <Anikatak@gmail.com>
2025-01-25 12:26:36 -0800

    Added Token

--- discordbot.py
+++ discordbot.py
@@ -1,10 +1,20 @@
 import discord 
+
 from discord.ext import commands
 
 intents = discord.Intents.default()
 intents.messages = True
+intents.message_content = True
 bot = commands.Bot(command_prefix = "~", intents = intents )
 
+@bot.command()
 async def command(ctx):
-    await ctx.send()
+    await ctx.send("hello! you used '~command'!")
+    print(yes)
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user}')
+
+bot.run('..')
     
